ae78d7c5e77cb66fc8b99547c75341809136add9
Brandon Castellano <Breakthrough@users.noreply.github.com>
2012-03-04 16:20:54 -0500

    Initial push of the driver development branch. This will be used to create a basic driver framework, for use with various other streaming technologies (e.g. serial/USB, UDP, TCP, or any other C++ compatible API).

---
+++ TCDriver.cpp
@@ -0,0 +1,25 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                     *
+ *                             TCDriver Object Source Code                             *
+ *                                      TRICLYSM                                       *
+ *                                                                                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                     *
+ *  This file contains the implementation of the TCDriver class as defined by the      *
+ *  TCDriver.h header file.  This class allows the current cube's voxel data to be     *
+ *  retrieved synchronously or asynchronously, to stream it to a physical LED cube.    *
+ *                                                                                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                     *
+ *  Copyright (C) 2011 Brandon Castellano, Ryan Mantha. All rights reserved.           *
+ *  Triclysm is provided under the BSD-2-Clause license. This program uses the SDL     *
+ *  (Simple DirectMedia Layer) library, and the Lua scripting language. See the        *
+ *  included LICENSE file or <http://www.triclysm.com/> for more details.              *
+ *                                                                                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+///
+/// \file  TCDriver.cpp
+/// \brief This file contains the implementation of the TCDriver class as defined by the
+///        TCDriver.h header file.
+///
