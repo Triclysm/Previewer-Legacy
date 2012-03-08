44a83cf9e33014c715111c16ce8809dafe5279e6
Brandon Castellano <Breakthrough@users.noreply.github.com>
2012-03-07 20:16:27 -0500

    The netdrv list command now works successfully, and can list all LED cubes on the network.  There is a lot of constants still in the code, and much work to be done, but this is indeed a major milestone (since the rest of the implementation should be straight-forward from here on).

--- drivers/netdrv.h
+++ drivers/netdrv.h
@@ -7,11 +7,30 @@
 #include "../TCCube.h"
 #include "SDL.h"
 #include "SDL_net.h"
+#include <vector>
 
 #define TC_FF_0C_888_BITPACK        0x01
 #define TC_FF_1C_888_CD4_BYTEPACK   0x02
 #define TC_FF_1C_888_CD8            0x03
 
+
+void netdrv_GetCubeList(Uint32 cube_ip, Uint16 cube_listenport,
+    Uint16 listenPort, unsigned int attempts = 3, Uint32 attempt_len_ms = 100);
+bool netdrv_ConnectCube(unsigned int cubeNum);
+
+struct cubeInfo
+{
+  public:
+    Uint16      cube_listenport;
+    Uint16      localport;
+    Uint32      cube_ip;
+    Uint8       cube_ffmt;
+    Uint8       cube_color;
+    byte        cube_size[3];
+    std::string cube_name;
+};
+
+extern std::vector<cubeInfo> cubeList;
 
 class TCDriver_netdrv : public TCDriver
 {
