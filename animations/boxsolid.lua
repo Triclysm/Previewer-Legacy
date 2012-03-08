b6f8991b475b3d30af27cfab8d5058e2b9cd0016
Ryan <Ryan@Ryans-PC>
2012-03-08 02:55:49 -0500

    Updated animations to all call "DoneIteration()", and removes "ticks" divison. Now when you wish to slow down an animation, use tickrate console command. The randomWalk.lua animation now only prints stats to files when argument '1' is provided. Anything else, including nothing will not print stats to files. Included a stats.tcs script. Running this will generate a stats run. Also, added a demo.tcs. It is Empty... for now!

--- animations/boxsolid.lua
+++ animations/boxsolid.lua
@@ -1,19 +1,19 @@
 loadfile("animbase.lua")(); SetNumColors(0)
 
-tick     = 0
 size     = 0
-speed    = 0
 corner   = nil
 mSize    = nil
 state    = true
+init     = false
 
-bitField = { {0,0,0},  {1,0,0},  {0,1,0},  {1,1,0},
-             {0,0,1},  {1,0,1},  {0,1,1},  {1,1,1}  }
+bitField = {{0,0,0}, {1,0,0}, 
+            {0,1,0}, {1,1,0}, 
+            {0,0,1}, {1,0,1}, 
+            {0,1,1}, {1,1,1}}
 
-function Initialize(_speed)
+function Initialize()
     math.randomseed(os.time())
 
-    speed  = _speed
     mSize  = MaxSize()
     corner = math.random(1, 8)
     
@@ -23,29 +23,27 @@ function Initialize(_speed)
 end
 
 function Update()
-    if (tick % speed) == 0 then
-        if (size == 0  and tick ~= 0) then 
-            corner = math.random(1, 8)
-            state = not state
-        end
+    if (size == 0  and init) then 
+        corner = math.random(1, 8)
+        state = not state
+        DoneIteration()
+    end
 
-        for x=0, math.min(size, sx-1) do
-            for y=0, math.min(size, sy-1) do
-                for z=0, math.min(size, sz-1) do
-                    SetVoxelState( T(X_AXIS, x), 
-                                   T(Y_AXIS, y), 
-                                   T(Z_AXIS, z),
-                                   state         )
-                end
+    for x=0, math.min(size, sx-1) do
+        for y=0, math.min(size, sy-1) do
+            for z=0, math.min(size, sz-1) do
+                SetVoxelState( T(X_AXIS, x), 
+                                T(Y_AXIS, y), 
+                                T(Z_AXIS, z),
+                                state         )
             end
         end
-
-        size = (size + 1) % mSize
     end
-    tick = tick + 1
+
+    size = (size + 1) % mSize
+    init = true
 end
 
--- Translated Voxels to proper position
 function T(axis, coord)
     if (bitField[corner][axis + 1] == 1) then 
         return (sc[axis] - 1) - coord
