379e3928c6ea36e71cfe9b633d9c97e9315e2ec8
Administrator <Administrator@BRANDON-W7i-PC>
2012-03-07 20:46:03 -0500

    Cube list and connections now work with the 8x8x8 cube. Now all that has to be done is to add support for sending commands, and connecting manually to a cube.
    Also, I encountered a strange bug - when the TCDriver_netdrv class' destructor
    is called, the SDLNet_FreePacket call causes an access violation. The same
    behaviour was noted when replacing the call with the delete operator. This
    needs to be further investigated, but to avoid an error message every time the
    program is closed, I changed udpPkt to be a static member of the
    TCDriver_netdrv class, and it is only allocated once.

--- drivers/netdrv.cpp
+++ drivers/netdrv.cpp
@@ -8,16 +8,14 @@
 #include "../main.h"
 #include "../console.h"
 
-TCDriver_netdrv::TCDriver_netdrv(
-    Uint32 cube_ip,  Uint16 cube_port, Uint16 listen_port,
-    bool &connected, Uint32 rate )
+TCDriver_netdrv::TCDriver_netdrv(cubeInfo &cube_params, bool &connected, Uint32 rate)
     : TCDriver(rate)
 {
     connected = false;
 
     // Create UDP sockets.
-    if (    !(sckSend = SDLNet_UDP_Open(cube_port))
-         || !(sckRecv = SDLNet_UDP_Open(listen_port)) )
+    if (    !(sckSend = SDLNet_UDP_Open(cube_params.cube_listenport))
+         || !(sckRecv = SDLNet_UDP_Open(cube_params.localport)) )
     {
         WriteOutput("netdrv: Error - could not initialize UDP sockets!");
         return;
@@ -29,68 +27,40 @@ TCDriver_netdrv::TCDriver_netdrv(
         return;
     }
     
-    cubeIp.host = cube_ip;
-    cubeIp.port = cube_port;
+    cubeIp.host = cube_params.cube_ip;
+    cubeIp.port = cube_params.cube_listenport;
 
     udpPkt->address.host = cubeIp.host;
     udpPkt->address.port = cubeIp.port;
 
+    // Store size & frame format.
+    frameFormat = cube_params.cube_ffmt;
+    remoteCubeSize[0] = cube_params.cube_size[0];
+    remoteCubeSize[1] = cube_params.cube_size[1];
+    remoteCubeSize[2] = cube_params.cube_size[2];
 
-    // Now attempt to retrieve cube parameters.
-    unsigned int attempts   = 3;
-    Uint32       attemptLen = 100,
-                 startTime;
-    std::string  cubeParams;
-    bool         gotParams = false;
-    for (unsigned int i = 0; i < attempts; i++)
-    {
-        SendCommand("*TP**TE*");
-        startTime = SDL_GetTicks();
-        while ((SDL_GetTicks() - startTime) < attemptLen)
-        {
-            if (RecvString(cubeParams) && ParseCubeParams(cubeParams))
-            {
-                // If we got the cube parameters, and they are valid,
-                // then we can break out of both loops.
-                gotParams = true;
-                i = attempts;
-                break;
-            }
-        }
-    }
-    // If we get here, everything went okay, so we can set connected
-    // to true, and write a message to the console stating that the
-    // driver is loaded.
-    if (gotParams)
-    {
-        // Update the current cube size to match the physical cube.
-        byte *currSize = GetCubeSize();
-        // We only need to change the size if there is a mis-match.
-        if ( !(    remoteCubeSize[0] == currSize[0]
-                && remoteCubeSize[1] == currSize[1]
-                && remoteCubeSize[2] == currSize[2] ))
-        {
-            SetCubeSize(remoteCubeSize[0], remoteCubeSize[1], remoteCubeSize[2]);
-        }
-        delete currSize;
 
-        // Finally, output that we are connected, and set the flag to true.
-        WriteOutput("netdrv: Connected to LED cube.");
-        connected = true;
-    }
-    else
+    // Update the current cube size to match the physical cube.
+    byte *currSize = GetCubeSize();
+    // We only need to change the size if there is a mis-match.
+    if ( !(    remoteCubeSize[0] == currSize[0]
+            && remoteCubeSize[1] == currSize[1]
+            && remoteCubeSize[2] == currSize[2] ))
     {
-        WriteOutput("netdrv: Error - could not connect to LED cube.");
-        WriteOutput("netdrv: Could not get parameters from remote IP.");
-        connected = false;
+        SetCubeSize(remoteCubeSize[0], remoteCubeSize[1], remoteCubeSize[2]);
     }
+    delete currSize;
+
+    // Finally, set the connected flag to true.
+    connected = true;
 }
 
 TCDriver_netdrv::~TCDriver_netdrv()
 {
-    SDLNet_FreePacket(udpPkt);
     SDLNet_UDP_Close(sckRecv);
     SDLNet_UDP_Close(sckSend);
+    //SDLNet_FreePacket(udpPkt); Need to sort out issues with this.
+    // Program crashes when closing, is something still using this packet?
 }
 
 
@@ -115,20 +85,6 @@ bool TCDriver_netdrv::RecvString(std::string &toRecv)
     {
         return false;
     }
-}
-
-bool TCDriver_netdrv::ParseCubeParams(const std::string &cube_params)
-{
-    // Parameter listing:
-    //  PN = Parameter Name
-    //  PD = Parameter Dimensions
-    //  PC = Parameter Color
-    //  PF = Parameter Frame Format
-    //  PE = Parameter End
-
-    // First, ensure that all parameters were received, and record the position
-    // of the last parameter.
-    return false;
 }
 
 
@@ -167,7 +123,10 @@ void TCDriver_netdrv::Poll()
     SendCommand(toSend);
 }
 
+
 std::vector<cubeInfo> cubeList;
+
+
 
 void netdrv_GetCubeList(Uint32 cube_ip, Uint16 cube_listenport,
     Uint16 localport, unsigned int attempts, Uint32 attempt_len_ms)
@@ -243,16 +202,33 @@ void netdrv_GetCubeList(Uint32 cube_ip, Uint16 cube_listenport,
                 // Check that they're spaced 3 chars apart (plus token length).
                 if ((tokenEnd - tokenStart) != (7)) break; // 7 = 3 + 4
                 // Finally, get the frame format.
-                tmpCube.cube_size[0] = (byte)cubeParams[tokenStart + 4];
-                tmpCube.cube_size[1] = (byte)cubeParams[tokenStart + 5];
-                tmpCube.cube_size[2] = (byte)cubeParams[tokenStart + 6];
+                tmpCube.cube_size[0] = (byte)cubeParams[tokenStart + 4] - '0';
+                tmpCube.cube_size[1] = (byte)cubeParams[tokenStart + 5] - '0';
+                tmpCube.cube_size[2] = (byte)cubeParams[tokenStart + 6] - '0';
 
                 // Finally, if we get here, add the remaining packet parameters to the
                 // temporary cube, and we can then add it to the list.
                 tmpCube.cube_listenport = cube_listenport;
                 tmpCube.localport       = localport;
                 tmpCube.cube_ip = udpPkt->address.host;
-                cubeList.push_back(tmpCube);
+                bool isDuplicate = false;
+                for (unsigned int i = 0; i < cubeList.size(); i++)
+                {
+                    // They are a match if all params agree.
+                    if (   cubeList[i].cube_listenport == tmpCube.cube_listenport
+                        && cubeList[i].localport       == tmpCube.localport
+                        && cubeList[i].cube_ip         == tmpCube.cube_ip
+                        && cubeList[i].cube_ffmt       == tmpCube.cube_ffmt
+                        && cubeList[i].cube_size[0]    == tmpCube.cube_size[0]
+                        && cubeList[i].cube_size[1]    == tmpCube.cube_size[1]
+                        && cubeList[i].cube_size[2]    == tmpCube.cube_size[2]
+                        && cubeList[i].cube_name       == tmpCube.cube_name         )
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate) cubeList.push_back(tmpCube);
             }
         }
     }
@@ -262,11 +238,34 @@ void netdrv_GetCubeList(Uint32 cube_ip, Uint16 cube_listenport,
 
 bool netdrv_ConnectCube(unsigned int cubeNum)
 {
+    TCDriver *toLoad    = NULL;
+    bool     connStatus = false;
+
     if (cubeList.size() == 0)
     {
         WriteOutput("netdrv: Error - no cubes found. "
                     "Please run `netdrv list` to scan for remote devices.");
         return false;
     }
-    return false;
+    else if (cubeNum < 0 || cubeNum > cubeList.size())
+    {
+
+        WriteOutput("netdrv: Error - invalid cube index.");
+        return false;
+    }
+
+    // attempt to load that driver.
+    cubeNum--;  // correct for 0-based index.
+    toLoad = new TCDriver_netdrv(cubeList[cubeNum], connStatus, 0);
+    if (connStatus == true)
+    {
+        SetDriver(toLoad);
+        WriteOutput("netdrv: Connected to LED cube.");
+        return true;
+    }
+    else
+    {
+        WriteOutput("netdrv: Error - could not connect to cube.");
+        return false;
+    }
 }
